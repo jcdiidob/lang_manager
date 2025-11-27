ec02c7ca474b1588999342700ae0f9480492b075
jcdiidob <idobarad@gmail.com>
2025-11-27 20:25:28 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 APP = ['language_monitor.py']
-DATA_FILES = []
+
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['psutil', 'keyboard'],
@@ -10,14 +10,13 @@ OPTIONS = {
         'CFBundleName': 'Language Monitor',
         'CFBundleDisplayName': 'Language Monitor',
         'CFBundleVersion': '1.0',
-        'CFBundleIdentifier': 'com.user.languagemonitor',
+        'CFBundleIdentifier': 'com.jcdidob.languagemonitor',
         'LSUIElement': True,
     }
 }
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
